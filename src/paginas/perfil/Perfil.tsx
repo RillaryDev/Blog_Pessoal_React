e353496135935f8e51587d76ca181f3c1d2731a2
RillaryDev <rillarydev@gmail.com>
2023-08-15 00:52:47 -0300

    final

--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/loginlogo.gif'
 import { toastAlerta } from '../../utils/toastAlerta'
 function Perfil() {
   let navigate = useNavigate()
@@ -17,7 +16,7 @@ function Perfil() {
     
   return (
     <div className='container mx-auto mt-0 rounded-2xl overflow-hidden'>
-      <img className='w-full h-72 object-cover border-b-8 border-white' src={loginLogo} alt="Capa do Perfil" />
+      <img className='w-full h-72 object-cover border-b-8 border-white' src="https://i.imgur.com/gbEm1H4.jpg" alt="Capa do Perfil" />
       <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
       <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-300 text-black text-2xl items-center justify-center">
         <p>Nome: {usuario.nome} </p>
